68eb2fef775e32bb673dd1c43e782d9bb505ad1f
Nisha9950 <nishapatel20011@gmail.com>
2025-03-19 12:54:38 +0530

    create client folder and create the controller and router and last is signin api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,39 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import mongoose from  'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
+});
+
 
 const app = express();
+app.use(cors())
+
+
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log("Server is running on port 3000! ")
+});
+
+app.use('/api/user', userRouter);
+app.use("/api/auth", authRouter);
+
+
+//middleware
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
